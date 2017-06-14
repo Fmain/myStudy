61da911fddff540f4f31df9fef1509ea50d55daf
Fmain <ukisssky@163.com>
2017-06-14 14:08:07 +0800

    开始cms项目

--- Vue/6 Vue实战/CMS/src/App.vue
+++ Vue/6 Vue实战/CMS/src/App.vue
@@ -1,12 +1,32 @@
 <template>
 	<div class="tmpl">
     <!-- header-->
+        <mt-header fixed title="CMS内容管理系统"></mt-header>
     <!-- end header-->
     <!-- content-->
         <!--路由占位-->
         <router-view></router-view>
     <!-- end content-->
     <!-- footer-->
+        <nav class="mui-bar mui-bar-tab">
+            <a class="mui-tab-item mui-active" href="#tabbar">
+                <span class="mui-icon mui-icon-home"></span>
+                <span class="mui-tab-label">首页</span>
+            </a>
+            <a class="mui-tab-item" href="#tabbar-with-chat">
+                <span class="mui-icon mui-icon-email"></span>
+                <span class="mui-tab-label">会员</span>
+            </a>
+            <a class="mui-tab-item" href="#tabbar-with-contact">
+                <span class="mui-icon mui-icon-contact">                                    <span class="mui-badge">3</span>
+                </span>
+                <span class="mui-tab-label">购物</span>
+            </a>
+            <a class="mui-tab-item" href="#tabbar-with-map">
+                <span class="mui-icon mui-icon-gear"></span>
+                <span class="mui-tab-label">搜索</span>
+            </a>
+        </nav>
     <!-- end footer-->
 	</div>
 </template>
@@ -24,5 +44,5 @@
 </script>
 
 <style>
-	
+
 </style>
--- Vue/6 Vue实战/CMS/src/main.js
+++ Vue/6 Vue实战/CMS/src/main.js
@@ -3,14 +3,12 @@ import Vue from 'vue'
 import vueRouter from 'vue-router'
 import vueResource from 'vue-resource'
 import mintUI from 'mint-ui'
-// 1.1 导入相关.vue组件
-import App from './App.vue'
-// 1.2 导入mint-ui的css
+// 1.1 导入mint-ui的css
 import 'mint-ui/lib/style.min.css'
-// 1.3.1 导入mui的css和字体图标样式
+// 1.2.1 导入mui的css和字体图标样式
 import '../statics/mui/css/mui.css'
 import '../statics/mui/css/icons-extra.css'
-// 1.3.2 导入mui相关的js
+// 1.2.2 导入mui相关的js
 import '../statics/mui/js/mui.min.js'
 
 // 2.0 在Vue中加载相关组件
@@ -18,8 +16,10 @@ Vue.use(vueRouter);
 Vue.use(vueResource);
 Vue.use(mintUI);
 
-// 3.0 定义路由规则对象
+// 3.0 导入相关.vue组件
+import App from './App.vue'
 import Home from './components/Home/Home.vue'
+// 3.1 定义路由规则对象
 let router = new vueRouter({
 	routes:[
 		{name:'Home',path:'/Home',component:Home},
