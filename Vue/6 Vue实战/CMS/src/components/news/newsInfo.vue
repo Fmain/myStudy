dd2252e9919e20d274cea8bd5315f68bbb00cd02
Fmain <ukisssky@163.com>
2017-06-19 14:49:59 +0800

    空了两天

--- Vue/6 Vue实战/CMS/src/components/news/newsInfo.vue
+++ Vue/6 Vue实战/CMS/src/components/news/newsInfo.vue
@@ -7,39 +7,42 @@
       {{newsInfo.click}}次浏览
       分类:经济民生
     </div>
-    <div class="content">
-      <div v-html="newsInfo.content"></div>
-    </div>
+    <div class="content"><div v-html="newsInfo.content"></div></div>
+    <!--评论组件-->
     <div>
-      <subcomment :comid="this.$route.params.id"></subcomment>
-      {{this.$route.params.id}}
+      <subcomment :artid="artid"></subcomment>
     </div>
   </div>
 </template>
+
 <script>
-import axios from 'axios'
-import subcomment from '../subcomp/subcomment.vue'
-export default{
-  data(){
-    return {
-      newsInfo: []
-    }
-  },
-  created(){
-    var url = 'http://webhm.top:8899/api/getnew/'+this.$route.params.id;
-    axios.get(url).then(response=>{
-      //console.log(response);
-      //注意,response.data.message为一个数组,需要添加索引
-      this.newsInfo = response.data.message[0];
-    }).catch(error=>{
-       console.log("404: "+error);
-    });
-  },
-  components: {
-    subcomment
-  },
-}
+  import axios from 'axios'
+  import subcomment from '../subcomp/subcomment.vue'
+
+  export default{
+    data(){
+      return {
+        newsInfo: [],
+        artid: 0
+      }
+    },
+    created(){
+      this.artid = this.$route.params.id;
+      var url = 'http://webhm.top:8899/api/getnew/'+this.artid;
+      axios.get(url).then(response=>{
+        //console.log(response);
+        //注意,response.data.message为一个数组,需要添加索引
+        this.newsInfo = response.data.message[0];
+      }).catch(error=>{
+         console.log("404: "+error);
+      });
+    },
+    components: {
+      subcomment
+    },
+  }
 </script>
+
 <style scoped>
 .title {
   height: 100%;
@@ -58,5 +61,6 @@ export default{
 }
 .content {
   padding: 5px;
+  overflow: hidden;
 }
 </style>
--- Vue/6 Vue实战/CMS/src/components/subcomp/subcomment.vue
+++ Vue/6 Vue实战/CMS/src/components/subcomp/subcomment.vue
@@ -1,28 +1,128 @@
 <template>
-  <div>
-    comment
+  <div class="tmplsub">
+    <div class="subminttitle">
+      <h4>提交评论</h4>
+    </div>
+    <div class="submintarea">
+      <textarea class="textarea" placeholder="请输入评论内容" v-model="contemt"></textarea>
+      <mt-button type="primary" size="large" @click="postcomment">发表</mt-button>
+    </div>
+
+    <div class="sublisttitle">
+      <h4>评论列表</h4>
+      1 <span>条评论</span>
+    </div>
+    <div>
+      <ul class="submitlist">
+        <li v-for="item in comments">
+          <div class="content" v-text="item.content"></div>
+          <div>
+            <span class="user" v-text="item.user_name"></span>
+            <span class="time">{{item.add_time | fmtdate('YYYY-MM-DD HH:mm:ss')}} </span>
+          </div>
+        </li>
+      </ul>
+    </div>
+
   </div>
 </template>
 <script>
+  import axios from 'axios'
   export default{
     data(){
       return {
+        contemt: '',
+        pageindex: 1,
         comments:[]
       }
     },
+    /*{
+      'content': '-block-block-block-block-block-block-block-block-block-block-block-block-block评论内容',
+      'user_name': '匿名用户',
+      'add_time': '2017-06-17 19:13:22'
+    }*/
     methods: {
       // 1.0 提交评论
       postcomment(){
-
+        let url = 'http://webhm.top:8899/api/postcomment/'+this.artid;
+        axios.post(url,{
+          artid: 14,
+          content: '测试post请求是否提交成功'
+        }).then(res=>{
+          console.log(res.data.message)
+        }).catch(err=>{
+          console.log('post请求失败')
+        })
       },
       // 2.0 获取评论
       getcomment(){
-
+        let url = 'http://webhm.top:8899/api/getcomments/'+ this.artid +'?pageindex='+ this.pageindex;
+        axios.get(url).then(res=>{
+          this.comments = res.data.message;
+        }).catch(err=>{
+          console.log("404: "+error);
+        });
       }
     },
-    porops: ['comid']
+    created(){
+      this.getcomment()
+    },
+    props: ['artid']
   }
 </script>
 <style scoped>
-
+  .tmplsub {
+  }
+  .subminttitle, .sublisttitle {
+    width: 100%;
+    line-height: 30px;
+    padding: 5px;
+    margin-top: 20px;
+    border-bottom: 1px solid rgba(92,92,92,0.4);
+  }
+  .subminttitle, .sublisttitle h4 {
+    display: inline-block;
+    width: 70%;
+    font-size: 20px;
+    font-weight: bold;
+  }
+  .sublisttitle span {
+    font-size: 14px;
+    color: rgba(92,92,92,0.8);
+  }
+  .submintarea {
+    padding: 10px 10px 30px 10px;
+    border-bottom: 1px solid rgba(92,92,92,0.4);
+  }
+  .textarea {
+    margin-top: 5px;
+    font-size: 14px;
+    margin-bottom: 5px;
+    height: 100px;
+  }
+  .submitlist {
+    list-style: none;
+    padding: 5px;
+    margin: 0 5px;
+  }
+  .submitlist li {
+    border-bottom: 1px solid rgba(92,92,92,0.4);
+    padding: 5px;
+  }
+  .content {
+    font-size: 15px;
+    margin-bottom: 10px;
+  }
+  .user {
+    display: inline-block;
+    color: #26a2ff;
+    font-size: 14px;
+    font-weight:600;
+    width: 45%;
+  }
+  .time {
+    display: inline;
+    font-size: 14px;
+    width: 40%;
+  }
 </style>
